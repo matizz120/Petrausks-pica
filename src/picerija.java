0c5cfbb6a6d0795c9671512b15921c33d130ab5a
Matīss Petrausks <matiss44555@gmail.com>
2021-02-17 19:00:38 +0200

    Pievienota izveidot metode

--- src/picerija.java
+++ src/picerija.java
@@ -1,7 +1,21 @@
 import javax.swing.JOptionPane;
 
 public class picerija {
-
+	
+	public static int izveidot(int pica){
+		int lielums = 0;
+			lielums = Integer.parseInt(JOptionPane.showInputDialog("Ievadi picas garumu/platumu (30, 40, 50 cm): "));
+			if(lielums > 50 || lielums < 30) {
+				JOptionPane.showMessageDialog(null, "Tâds lielums nepastâv! (30, 40, 50 cm!)");
+			}else if(lielums==30 || lielums==40 || lielums==50) {
+				JOptionPane.showMessageDialog(null, "Picas lielums ir "+lielums);
+			}else {
+				JOptionPane.showMessageDialog(null, "Tâds lielums nepastâv! (30, 40, 50 cm!)");
+			}
+			pica = lielums;
+		return pica;
+	}
+	
 	public static void main(String[] args) {
 		String izvele;
 		int pica = 0;
